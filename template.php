ef62f06dcabcb178a315cc52f9fa0c9d27f4fd8c
Luiz Carvalho <projetodc@gmail.com>
2016-03-11 18:08:14 -0300

    remove system css

--- template.php
+++ template.php
@@ -535,6 +535,9 @@ function avaliacao_digital_theme_paths($theme) {
 
 function avaliacao_digital_css_alter(&$css) {
   unset($css[drupal_get_path('module','system').'/system.theme.css']);
+  unset($css[drupal_get_path('module','system').'/system.base.css']);
+  unset($css[drupal_get_path('module','system').'/system.menus.css']);
+  unset($css[drupal_get_path('module','system').'/system.messages.css']);
   unset($css[drupal_get_path('module','toolbar').'/toolbar.css']);
   unset($css[drupal_get_path('theme','fusion_core').'/css/fusion-typography.css']);
   unset($css[drupal_get_path('theme','fusion_core').'/css/grid16-960.css']);
